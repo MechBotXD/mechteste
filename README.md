1d5fb74313e422f147ae6f684ec34fd3fa702de1
unknown <jmpacheco310@gmail.com>
2021-01-26 20:57:43 -0300

    commit um

---
+++ README.md
@@ -0,0 +1,45 @@
+### Opa, aqui é o Specc :)
+<a href="           " >
+
+<br>
+
+
+
+
+<summary>🍙 Ajuda!</summary>
+
+## Itens necessários
+
+```bash
+> Termux
+> WhatsApp
+> Celulares (2)
+```
+
+---
+
+
+## Método de Instalação
+Siga os passos abaixo!
+
+```bash
+> termux-setup-storage
+(Permita o Acesso ao armazenamento)
+> apt install git -y
+> git clone (                              )
+> cd termux-whatsapp-bot
+> bash install.sh
+```
+
+## Use
+
+```bash
+> npm start
+```
+
+## Notas do Patch
+```
+Nada ainda...
+```
+
+<img src="">
---
+++ src/help.js
@@ -0,0 +1,109 @@
+const help = (prefix, pushname, botname, oname) => {
+	return `
+  「 *Specc Bot* 」
+  :)
+  
+  ✇ *IMPORTANTE*
+    ∞ Criador: Specc
+    ∞ Prefixo: 「  ${prefix}  」
+    ∞ Canal do Bot: 
+    ∞ Grupo do Bot: 
+    ∞ Grupo do dono do Bot: 
+    ∞ Como instalar o Bot Passo a Passo:
+    
+    Se divirta com os menus abaixo:
+
+    ∞${prefix}nsfwmenu
+    ∞${prefix}menuadmin
+    ∞${prefix}toinmenu
+  
+    
+  ✇ *NÍVEL*
+    │
+    ├─ ∞ ${prefix}leveling
+    └─ ∞ ${prefix}level
+  ✇ *ÁREA DE CRIAÇÃO*
+    │
+    ├─ ∞ ${prefix}sticker
+    ├─ ∞ ${prefix}toimg
+    ├─ ∞ ${prefix}bpink
+    ├─ ∞ ${prefix}marvellogo
+    ├─ ∞ ${prefix}snowwrite
+    ├─ ∞ ${prefix}3dtext
+    ├─ ∞ ${prefix}ninjalogo
+    ├─ ∞ ${prefix}water
+    ├─ ∞ ${prefix}firetext
+    ├─ ∞ ${prefix}neonlogo
+    ├─ ∞ ${prefix}lionlogo
+    ├─ ∞ ${prefix}jokerlogo
+    └─ ∞ ${prefix}quotemaker
+  ✇ *COMANDOS DE PESQUISA*
+    │
+    ├─ ∞ ${prefix}wiki
+    ├─ ∞ ${prefix}wikien
+    ├─ ∞ ${prefix}nulis
+    └─ ∞ ${prefix}artinama
+  ✇ *BAIXAR :)*
+    │
+    ├─ ∞ ${prefix}images 
+    ├─ ∞ ${prefix}ytmp3
+    ├─ ∞ ${prefix}ytmp4
+    ├─ ∞ ${prefix}joox
+    ├─ ∞ ${prefix}joox2
+    ├─ ∞ ${prefix}trendtwit
+    └─ ∞ ${prefix}ytsearch
+  ✇ *MEMEZINHOS*
+    │
+    ├─ ∞ ${prefix}meme
+    └─ ∞ ${prefix}memeindo
+  ✇ *SONS*
+    │
+    ├─ ∞ ${prefix}play
+    └─ ∞ ${prefix}tts
+  ✇ *MUSICAS*
+    │
+    ├─ ∞ ${prefix}lirik
+    └─ ∞ ${prefix}chord
+  ✇ *STALKEAR*
+    │
+    ├─ ∞ ${prefix}tiktokstalk
+    └─ ∞ ${prefix}igstalk
+  ✇ *WIBU*
+    │
+    ├─ ∞ ${prefix}neonime
+    ├─ ∞ ${prefix}pokemon
+    ├─ ∞ ${prefix}loli
+    ├─ ∞ ${prefix}waifu
+    ├─ ∞ ${prefix}wallpaper
+    ├─ ∞ ${prefix}neko
+    └─ ∞ ${prefix}nekonime
+  ✇ *DIVERSÃO*
+    │
+    ├─ ∞ ${prefix}alay
+    ├─ ∞ ${prefix}gantengcek
+    ├─ ∞ ${prefix}watak
+    ├─ ∞ ${prefix}hobby
+    ├─ ∞ ${prefix}game
+    ├─ ∞ ${prefix}simih
+    └─ ∞ ${prefix}glitch
+  ✇ *INFORMAÇÂO*
+    │
+    ├─ ∞ ${prefix}bahasa
+    ├─ ∞ ${prefix}kodenegara
+    ├─ ∞ ${prefix}kbbi
+    ├─ ∞ ${prefix}fakta
+    ├─ ∞ ${prefix}infocuaca
+    └─ ∞ ${prefix}covid
+  ✇ *OUTROS*
+    │
+    ├─ ∞ ${prefix}send
+    ├─ ∞ ${prefix}wame
+    ├─ ∞ ${prefix}virtex
+    ├─ ∞ ${prefix}exe
+    ├─ ∞ ${prefix}qrcode
+    ├─ ∞ ${prefix}quotes
+    └─ ∞ ${prefix}fml
+  `
+}
+
+exports.help = help
